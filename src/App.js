89757dea35e7b97c5721f59a638a9cc22da31f89
Helena Makrenek <hmakrenek@gmail.com>
2019-11-27 21:57:04 +0100

    semantic ui added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Button } from 'semantic-ui-react';
 
 function App() {
   return (
@@ -8,7 +9,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <Button> Add </Button> and save to reload.
         </p>
         <a
           className="App-link"
@@ -19,6 +20,7 @@ function App() {
           Learn React
         </a>
       </header>
+
     </div>
   );
 }
