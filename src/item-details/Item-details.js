6a66114b8c0d3467a446f04d21832cc129f1e7d0
Adam Brudzynski <adam.brudzynski@gmail.com>
2020-01-13 22:08:53 +0100

    FZ12OK-44 basic element view done, added images and jsons modified by adding links to images

--- src/item-details/Item-details.js
+++ src/item-details/Item-details.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import { Button, Icon, Image, Item, Label } from 'semantic-ui-react'
 
-export default function ItemDetails (props){
+export default function ItemDetails(props) {
+    const { item } = props.location.state
     return (
-        <div> ItemDetails Component {props.itemId}</div>
+        <Item.Group relaxed>
+            <Item>
+                <Item.Image src={item.image ? item.image : null} />
+                <Item.Content>
+                    <Item.Header as='a'>{item.img ? item.img : null} {item.name}</Item.Header>
+                    <Item.Meta>
+                        <span>{item.description}</span>
+                    </Item.Meta>
+                    <Item.Description>Tip: {item.proTip}</Item.Description>
+                    <Item.Extra>
+                        <Button color='green' floated='right'>
+                            Add to list
+            <Icon name='right plus' />
+                        </Button>
+                    </Item.Extra>
+                </Item.Content>
+            </Item>
+
+        </Item.Group>
     )
 }
