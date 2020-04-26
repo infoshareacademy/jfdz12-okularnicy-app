f65dba729f3d2403af3cbd60d1282a2e342a3d9f
Adam Brudzynski <adam.brudzynski@gmail.com>
2020-04-26 13:16:33 +0200

    new DB configuration

--- src/index.js
+++ src/index.js
@@ -8,15 +8,14 @@ import App from './App';
 import * as serviceWorker from './serviceWorker';
 import { AuthContext } from './auth/Auth';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyCZXhm3zcG9vYBUhqKicIHKrcf1teHS4uM",
-    authDomain: "okularnicy-app.firebaseapp.com",
-    databaseURL: "https://okularnicy-app.firebaseio.com",
-    projectId: "okularnicy-app",
-    storageBucket: "okularnicy-app.appspot.com",
-    messagingSenderId: "91894787110",
-    appId: "1:91894787110:web:51e75bb16c6c56a0b5cae7",
-    measurementId: "G-TQ02ZKRZB4"
+var firebaseConfig = {
+  apiKey: "AIzaSyCBBzR29M0PB2JgHOHPurLDoYEmapMHUxU",
+  authDomain: "packmeapp-77c66.firebaseapp.com",
+  databaseURL: "https://packmeapp-77c66.firebaseio.com",
+  projectId: "packmeapp-77c66",
+  storageBucket: "packmeapp-77c66.appspot.com",
+  messagingSenderId: "1034465593756",
+  appId: "1:1034465593756:web:7df35e436960387cfbe059"
   };
   firebase.initializeApp(firebaseConfig);
 
